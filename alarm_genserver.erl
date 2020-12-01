675de84966a0266b8c0910ba9298ef006ffa5790
truongluu-antbuddy <75018176+truongluu-antbuddy@users.noreply.github.com>
2020-12-01 13:39:11 +0700

    update test erlang:time_after

--- alarm_genserver.erl
+++ alarm_genserver.erl
@@ -1,26 +1,30 @@
 -module(alarm_genserver).
 -behaviour(gen_server).
 
--export([start_link/1, stop/1]).
+-export([start_link/1, stop/1, send_after/1]).
 -export([init/1,
 	handle_call/3,
 	handle_cast/2,
 	handle_info/2,
 	code_change/3,
 	terminate/2]).
-%======= API function ==============
+-define(TIME, 10000).
+%===================== API function =====================
+send_after(_State) ->
+	io:format("before send~n"),
+	erlang:send_after(?TIME, _State, start_link(_State)).
+	%io:format("after send~n").
 start_link(State) -> %spawn and link gen_server State
 	gen_server:start_link({local,State}, ?MODULE, State, []).
+	%erlang:send_after(20000, self(), State).
 
 stop(State) -> % stop gen_server process
 	gen_server:call(State, stop).
 
-%======= Call back function ========
+%================== Call back function ==================
 init(State) -> % initialize new process
-	%process_flag(trap_exit, true),
-	io:format("Init here!! State = ~p~n",[State]),
-	%A = lists:last(State),
-	%io:format("last element of State = ~p~n", [A]),
+	%io:format("State Pid = ~p, self = ~p ~n",[whereis(State),self()]),
+	io:format("Init here on genserver!! State = ~p~n",[State]),
 	{ok,State,200}.
 
 handle_call(stop, _From, State) -> %handle stop(State)
@@ -36,10 +40,10 @@ handle_cast(_Message, State) -> %handle function genserver:cast
     	{noreply, State, 700}.
 
 handle_info(timeout, alarm) -> % handle with State = alarm, next state is turning
-	io:format("Ringging!!!~n"),
+	io:format("    Ringging !!!~n"),
 	{noreply,turning,2000};
 handle_info(timeout, turning) -> % handle with State = turning, next state is alarm
-	io:format("Shaking   !!!~n"),
+	io:format(" Shaking   !!!~n"),
 	{noreply,alarm,1000};
 
 handle_info(_Message, State)-> % handle function init, next State is alarm
@@ -50,6 +54,6 @@ code_change(_OldVsn, State, _Extra) -> %update internal state
     	{ok, State}.
 
 terminate(_Reason, _State) -> % stop genserver
-	io:format("This is terminal~n"),
+	io:format("This is terminal on genserver~n"),
     	ok.
 %send_after(Time, ?MODULE, Msg)
