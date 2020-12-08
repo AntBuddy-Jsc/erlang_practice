04a71d66cb686b5e7c1865df840c7afa4eb3c0e7
truongluu-antbuddy <75018176+truongluu-antbuddy@users.noreply.github.com>
2020-12-08 14:51:36 +0700

    clean code

--- test_ets.erl
+++ test_ets.erl
@@ -2,38 +2,36 @@
 -compile(export_all).
 -record(person, {name, age, phone, email}).
 
-create(_name, P=#person{}) -> %create and set first value for table _name
-%	io:format("before create~n"),
-%	ets:new(_name, [set, named_table, {keypos,1}]),
-	ets:new(_name, [bag, named_table, {keypos,1}]), %default keypos=1
-	ets:insert(_name, {P#person.name, P#person.age, P#person.phone, P#person.email}).
+create(Name) -> 									%create table Name
+	ets:new(Name, [bag, named_table, {keypos,1}]). 	%default keypos=1
 
-	%io:format("after create~n").
+insert(Name, P=#person{}) -> 						%insert new object for table Name
+	ets:insert(Name, {P#person.name, P#person.age, P#person.phone, P#person.email}).  
 
-insert(_name, P=#person{}) -> %insert new object for table _name
-%	io:format("Ten per = ~p~n",[P#person.name]),
-%	ets:insert_new(_name, {P#person.name, P#person.age, P#person.phone, P#person.email}).
-	ets:insert(_name, {P#person.name, P#person.age, P#person.phone, P#person.email}). %use for insert object into [bag] table
-%	io:format("After insert~n").
-
-search(_name, P=#person{}) -> %search name person P in _name, mean key 
+search(Name, P=#person{}) -> 						%search name person P in Name, mean key 
 	io:format("Search person ~p~n",[P#person.name]),
-	ets:lookup(_name, P#person.name);
-search(_name, _arg) -> %search key=_arg
+	ets:lookup(Name, P#person.name);
+
+search(Name, Arg) -> 								%search key=_arg
 	io:format("Search _arg~n"),
-	ets:lookup(_name, _arg).
-person(_name, _age, _phone, _email) -> %return a record
+	ets:lookup(Name, Arg).
+
+person(Name, Age, Phone, Email) -> 					%return a record
 	if 
-	   is_number(_age) ->
-		Person = #person{name=_name, age=_age, phone=_phone, email=_email},
-		Person;
-	   true ->
-		io:format("Wrong data type!")
+		is_number(Age) ->
+			Person = #person{name=Name, age=Age, phone=Phone, email=Email},
+			Person;
+		true ->
+			io:format("Wrong data type!")
 	end.
 
-is_person(_a) -> is_record(_a, person). %check if a is a person record
+is_person(Arg) -> is_record(Arg, person). 			%check if Arg a is a person record
 
+print(Table) -> 									%print list data in table
+	io:format("Table is: ~p~n",[ets:tab2list(Table)]).
 
+%=================================================================================
+%data to test
 %c(test_ets).
 %rr(test_ets).
 %P1=test_ets:person(ten, 23, 89393, mail1).
